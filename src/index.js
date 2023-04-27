f35cd8b41b9dd66682044fb8359a28b71b1d28ed
xiaoqi.wang <wxqmailbox@hotmail.com>
2023-04-27 18:16:07 +0900

    2023/04/27 First Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Todo from './todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
 
